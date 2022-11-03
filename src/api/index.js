588f38bb496217eae40c7eaf5ad6f582f224e13a
John Boyle <nikolasboyle@gmail.com>
2022-11-03 11:23:37 -0700

    Fix base url attempt 2

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
-const baseURL =
-  process.env.NODE_ENV === "development"
-    ? "https://jnb-api.ngrok.io"
-    : "https://perfect-trip.herokuapp.com";
+// const baseURL =
+//   process.env.NODE_ENV === "development"
+//     ? "https://jnb-api.ngrok.io"
+//     : "https://perfect-trip.herokuapp.com";
+
+const baseURL = "https://perfect-trip.herokuapp.com";
 
 export const getPlaces = async (query) => {
   try {
