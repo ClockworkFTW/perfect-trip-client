49897cc4b93d2ba3a5aa6ebcbfb14f90919265a3
ianwissy <wysei@oregonstate.edu>
2022-11-22 16:01:21 -0800

    updated main page and moved links to nav header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,10 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import Experiences from "../components/Experiences";
+import TripMap from "../components/Map/TripMap";
+
+import * as API from "../api";
 
 // Components
 import Button from "../components/Button";
@@ -11,7 +15,19 @@ import { UserContext } from "../App";
 
 const Home = () => {
   const [user] = useContext(UserContext);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
+  // Experience list state
+  const [experiences, setExperiences] = useState([]);
+
+  // Experience filter state
+  const [keywords, setKeywords] = useState([]);
+  const [coordinates, setCoordinates] = useState({
+    center: { latitude: 37.7749, longitude: -122.4194 },
+    northEast: null,
+    southWest: null,
+  });
   const navigate = useNavigate();
 
   const onCreateTripClicked = () => {
@@ -22,34 +38,60 @@ const Home = () => {
     navigate("/experience/edit/new");
   };
 
-  return user ? (
+  useEffect(() => {
+    const getExperiences = async () => {
+      try {
+        setLoading(true);
+        const result = await API.searchExperiences({ keywords, coordinates });
+        setExperiences(result);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (coordinates.northEast && coordinates.southWest) {
+      getExperiences();
+    }
+  }, [keywords, coordinates]);
+
+  return (
     <Container>
-      <h1>Home</h1>
-      <div>
-        <Button onClick={onCreateTripClicked}>
-          <Icon icon="plus" margin="0 8px 0 0" />
-          Plan New Trip
-        </Button>
-      </div>
-      <div>
-        <Button onClick={onCreateExperienceClicked}>
-          <Icon icon="plus" margin="0 8px 0 0" />
-          Create New Experience
-        </Button>
-      </div>
-    </Container>
-  ) : (
-    <Container>
-      <h1>Home</h1>
-      <p>Please register an account to create trips and experiences.</p>
+      <Sidebar>
+        <Experiences
+          loading={loading}
+          experiences={experiences}
+          keywords={keywords}
+        />
+      </Sidebar>
+      <Main>
+        <TripMap
+          loading={loading}
+          experiences={experiences}
+          latitude={coordinates.center.latitude}
+          longitude={coordinates.center.longitude}
+          setCoordinates={setCoordinates}/>
+      </Main>
     </Container>
   );
 };
 
 const Container = styled.div`
-  max-width: 1200px;
-  margin: 0 auto;
-  padding: 10px 30px;
+  height: 100%;
+  display: grid;
+  grid-template-columns: 320px 1fr;
 `;
+
+const Sidebar = styled.div`
+  height: 100%;
+  display: grid;
+  grid-template-columns: 320px;
+`;
+
+const Main = styled.div`
+  position: relative;
+`;
+
 
 export default Home;
