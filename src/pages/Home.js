d89c1391cce99128555f5da8019813f0afd041de
John Boyle <nikolasboyle@gmail.com>
2022-11-23 20:19:59 -0800

    Fix home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -40,6 +40,14 @@ const Wrapper = styled.div`
   position: relative;
   width: 100%;
   height: 100%;
+  background-color: ${({ theme }) => theme.neutral["800"]};
+`;
+
+const Container = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 `;
 
 const Header = styled.h1`
